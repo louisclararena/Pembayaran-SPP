e16157005bfefa95c80c70b87713d537c18fe438
louisclararena <63798481+louisclararena@users.noreply.github.com>
2021-06-04 23:08:12 +0700

    Add files via upload

---
+++ siswa-pembayaran-spp/src/Config.js
@@ -0,0 +1,9 @@
+const base_url = "http://localhost:8000/bayar/api/v1"
+const siswa_image_url = "http://localhost:8000/image/"
+const petugas_image_url = "http://localhost:8000/petugas_image/"
+
+export {
+    base_url,
+    siswa_image_url,
+    petugas_image_url
+}
